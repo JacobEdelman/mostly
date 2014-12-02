ed42753c9f852f1052d3bb7736f13c9da91d8e33
JacobEdelman <edelman.jd@gmail.com>
2014-12-02 18:31:22 -0500

    first

---
+++ combinators.js
@@ -0,0 +1,58 @@
+function lit(x){//for any letter,regex, or number
+  return function(y){
+    matched=y.match(x);
+    return {match:(matched && matched[0]===y),val:(matched && matched[0]),rightOverflow:"",leftOverflow:""};//not lists
+  }
+}
+var none=lit("");
+function or(x,y){
+  return function(z){
+    var tx=x(z);
+    if(tx.match)return tx;
+    var ty=y(z);
+    if(ty.match)return ty;
+    return {match:false}
+  }
+}
+function than(x,y){//so by memoizing this should turn into nearley effectively...
+  function zMatcher(z){//does this even need to use proto?
+    if(zMatcher.prototype.memoizer[z])return zMatcher.prototype.memoizer[z];//kinda slow but I don't think too much
+    else zMatcher.prototype.memoizer[z]={match:false};//this prevents useless recursing
+    var tx,ty;
+    for(var i=0;i<=z.length;i++){
+      tx=x(z.slice(0,i));
+      if(tx.match){
+        ty=y(tx.rightOverflow+z.slice(i));//not that there is no overflow of nonterminals
+        if(ty.match)break;
+      }
+      ty=y(z.slice(i));
+      if(ty.match && ty.leftOverflow){//this should prevent annoying things where rightOverflow hasn't works and we don't want to re try it
+        tx=x(z.slice(0,i)+ty.leftOverflow);//not that there is no overflow of nonterminals
+        if(tx.match)break;
+      }
+    }
+    if(tx.match && ty.match)zMatcher.prototype.memoizer[z]={val:[tx.val,ty.val],match:true,rightOverflow:ty.rightOverflow,leftOverflow:tx.leftOverflow};//good enough
+    return zMatcher.prototype.memoizer[z];
+  }
+  zMatcher.prototype.memoizer={};
+  return zMatcher;
+}
+function cat(){
+  var ret=arguments[Object.keys(arguments).slice(-1)];
+  for(var i=arguments.length-2;i>=0;i--)ret=than(arguments[i],ret);
+  return ret;
+}
+function context(l,x,r){
+  return function(){
+    var ret=x.apply(null,arguments);//no this I guess?
+    if(ret.match){
+      ret.rightOverflow+=r;
+      ret.leftOverflow+=l;
+    }
+    return ret;
+  }
+}
+var rules={};
+rules.ident=context("a",or(lit(/[a-zA-Z_$]/),cat(lit(/[a-zA-Z_$0-9]/),function(){return rules.ident.apply(null,arguments);})),'b');
+console.log(JSON.stringify(rules.ident("ab")));
+///hmmm... so can it parse anything? well in checking to see if something is parsable mine might just add enough to the sides to go on forever... yes I think it is that powerfu;
